8333e191e5c70ee91943b69badf65a5976bfb76f
Max Charles <m.charles@ieee.org>
2023-10-24 21:18:50 -0400

    Update script with ad-hoc commands.

--- data/myproject/commands.sh
+++ data/myproject/commands.sh
@@ -4,5 +4,23 @@ ansible all -m ansible.bultin.command -a 'uptime'
 # Install Package (i.e. install Nginx)
 ansible all -m ansible.builtin.package -a 'name=nginx state=present' -b
 
+# Ping server(s) by IP address
+ansible <ip_addr> -m ansible.bultin.ping
+ansible <ip_addr>,<ip_addr> -m ansible.bultin.ping
+
+# Check how memory the server has
+ansible all -m ansible.builtin.command -a 'free -mh' 
+# Command module is not executing commands using shell on your managed nodes, instead it uses Python,
+# which is why environment variables and also |,>,<, &&, || and ; won't work.
+ansible all -m ansible.builtin.command -a 'free -mh;date' # won't work
+
+# A command module is faster than a shell module as it is using Python to execute commands
+
+ansible all -m ansible.builtin.shell -a 'free -mh'
+# Shell Module is executing commands using shell on managed nodes, so environment variables and 
+# also |, >,<, &&, || and ; work properly
+
+ansible all -m ansible.buildin.shell -a 'free -mh;date'
+
 # Run playbook
 ansible-playbook demo.yml
